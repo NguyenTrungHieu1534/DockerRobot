f85f87e5111230453d9184206471efbfdb02e1e9
NguyenTrungHieu1534 <muselk35@gmail.com>
2026-03-16 14:59:06 +0700

    Self introduction

--- src/main/java/com/example/ngtrunghieu/controller/HomeController.java
+++ src/main/java/com/example/ngtrunghieu/controller/HomeController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "home/index";
     }
 }
