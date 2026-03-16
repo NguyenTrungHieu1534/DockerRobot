a59fb52b2142d161a18c3c4d9420346c3391242e
NguyenTrungHieu1534 <muselk35@gmail.com>
2026-03-16 15:07:17 +0700

    Update

--- src/main/java/com/example/ngtrunghieu/NgtrunghieuApplication.java
+++ src/main/java/com/example/ngtrunghieu/NgtrunghieuApplication.java
@@ -2,13 +2,7 @@ package com.example.ngtrunghieu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/**
- * Main application class for the Ngtrunghieu application.
- * This class is responsible for starting the Spring Boot application.
- * @author Nguyen Trung Hieu
- * @version 1.0
- * @since 2026-03-16
- */
+
 @SpringBootApplication
 public class NgtrunghieuApplication {
 
